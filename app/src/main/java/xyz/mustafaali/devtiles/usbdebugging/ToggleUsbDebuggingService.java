c000b23dd152653b2ddb758ea03cb31886df563c
Mustafa Ali <mustafa.ali@mutualmobile.com>
2016-12-27 15:24:34 +0530

    Updated main screen message

--- app/src/main/java/xyz/mustafaali/devtiles/usbdebugging/ToggleUsbDebuggingService.java
+++ app/src/main/java/xyz/mustafaali/devtiles/usbdebugging/ToggleUsbDebuggingService.java
@@ -5,49 +5,51 @@ import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.util.Log;
 import android.widget.Toast;
+
 import xyz.mustafaali.devtiles.R;
 
 /**
- * @author Mustafa Ali, 27/12/16.
+ * Tile Service to toggle USB Debugging.
  */
-
 public class ToggleUsbDebuggingService extends TileService {
 
-  private final String TAG = this.getClass().getSimpleName();
+    private final String TAG = this.getClass().getSimpleName();
 
-  @Override public void onStartListening() {
-    super.onStartListening();
-    updateTile();
-  }
-
-  @Override public void onClick() {
-    String newValue = isUsbDebuggingEnabled() ? "0" : "1";
-
-    try {
-      Settings.Global.putString(getContentResolver(), Settings.Global.ADB_ENABLED, newValue);
-    } catch (SecurityException se) {
-      String message = getString(R.string.permission_required_toast);
-      Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-      Log.e(TAG, message);
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateTile();
     }
 
-    updateTile();
-  }
+    @Override
+    public void onClick() {
+        String newValue = isUsbDebuggingEnabled() ? "0" : "1";
 
-  private boolean isUsbDebuggingEnabled() {
-    return Settings.Global.getString(getContentResolver(), Settings.Global.ADB_ENABLED).equals("1");
-  }
+        try {
+            Settings.Global.putString(getContentResolver(), Settings.Global.ADB_ENABLED, newValue);
+        } catch (SecurityException se) {
+            String message = getString(R.string.permission_required_toast);
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            Log.e(TAG, message);
+        }
 
-  private void updateTile() {
-    final Tile tile = getQsTile();
-
-    if (isUsbDebuggingEnabled()) {
-      tile.setState(Tile.STATE_ACTIVE);
-    } else {
-      tile.setState(Tile.STATE_INACTIVE);
+        updateTile();
     }
 
-    tile.updateTile();
+    private boolean isUsbDebuggingEnabled() {
+        return Settings.Global.getString(getContentResolver(), Settings.Global.ADB_ENABLED).equals("1");
+    }
 
-  }
+    private void updateTile() {
+        final Tile tile = getQsTile();
+
+        if (isUsbDebuggingEnabled()) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+
+        tile.updateTile();
+
+    }
 }
