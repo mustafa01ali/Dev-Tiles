057fce43c36f9557d96d6a105d92af038168060e
Mustafa Ali <mustafa.ali@mutualmobile.com>
2016-12-28 12:32:02 +0530

    Keeping screen on now works as expected, modified implementation to turn screen off if the device is connected to a wall charger

--- app/src/main/java/xyz/mustafaali/devtiles/service/ToggleKeepScreenOnService.java
+++ app/src/main/java/xyz/mustafaali/devtiles/service/ToggleKeepScreenOnService.java
@@ -1,5 +1,6 @@
 package xyz.mustafaali.devtiles.service;
 
+import android.os.BatteryManager;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
@@ -8,6 +9,11 @@ import android.widget.Toast;
 
 import xyz.mustafaali.devtiles.R;
 
+/**
+ * Tile Service to keep the screen on when the device is connected to a USB port on a computer.
+ *
+ * If the device is connected to a wall charger, the screen will turn off.
+ */
 public class ToggleKeepScreenOnService extends TileService {
     private final String TAG = this.getClass().getSimpleName();
 
@@ -19,7 +25,7 @@ public class ToggleKeepScreenOnService extends TileService {
 
     @Override
     public void onClick() {
-        int newValue = isKeepScreenOnEnabled() ? 0 : 1;
+        int newValue = isKeepScreenOnEnabled() ? 0 : BatteryManager.BATTERY_PLUGGED_USB;
 
         try {
             Settings.Global.putInt(getContentResolver(), Settings.Global.STAY_ON_WHILE_PLUGGED_IN, newValue);
@@ -34,7 +40,7 @@ public class ToggleKeepScreenOnService extends TileService {
 
     private boolean isKeepScreenOnEnabled() {
         try {
-            return Settings.Global.getInt(getContentResolver(), Settings.Global.STAY_ON_WHILE_PLUGGED_IN) == 1;
+            return Settings.Global.getInt(getContentResolver(), Settings.Global.STAY_ON_WHILE_PLUGGED_IN) == BatteryManager.BATTERY_PLUGGED_USB;
         } catch (Settings.SettingNotFoundException e) {
             Log.e(TAG, e.getMessage());
         }
