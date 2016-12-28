5ce1dcbf6094d15ca5a12a955dd63bd5f6258226
Mustafa Ali <mustafa.ali@mutualmobile.com>
2016-12-28 12:52:44 +0530

    Refactored common code

--- app/src/main/java/xyz/mustafaali/devtiles/service/ToggleKeepScreenOnService.java
+++ app/src/main/java/xyz/mustafaali/devtiles/service/ToggleKeepScreenOnService.java
@@ -2,43 +2,30 @@ package xyz.mustafaali.devtiles.service;
 
 import android.os.BatteryManager;
 import android.provider.Settings;
-import android.service.quicksettings.Tile;
-import android.service.quicksettings.TileService;
 import android.util.Log;
-import android.widget.Toast;
-
-import xyz.mustafaali.devtiles.R;
 
 /**
  * Tile Service to keep the screen on when the device is connected to a USB port on a computer.
  *
  * If the device is connected to a wall charger, the screen will turn off.
  */
-public class ToggleKeepScreenOnService extends TileService {
+public class ToggleKeepScreenOnService extends BaseTileService {
     private final String TAG = this.getClass().getSimpleName();
 
     @Override
-    public void onStartListening() {
-        super.onStartListening();
-        updateTile();
-    }
-
-    @Override
     public void onClick() {
-        int newValue = isKeepScreenOnEnabled() ? 0 : BatteryManager.BATTERY_PLUGGED_USB;
+        int newValue = isFeatureEnabled() ? 0 : BatteryManager.BATTERY_PLUGGED_USB;
 
         try {
             Settings.Global.putInt(getContentResolver(), Settings.Global.STAY_ON_WHILE_PLUGGED_IN, newValue);
         } catch (SecurityException se) {
-            String message = getString(R.string.permission_required_toast);
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-            Log.e(TAG, message);
+            showPermissionError();
         }
-
         updateTile();
     }
 
-    private boolean isKeepScreenOnEnabled() {
+    @Override
+    protected boolean isFeatureEnabled() {
         try {
             return Settings.Global.getInt(getContentResolver(), Settings.Global.STAY_ON_WHILE_PLUGGED_IN) == BatteryManager.BATTERY_PLUGGED_USB;
         } catch (Settings.SettingNotFoundException e) {
@@ -47,16 +34,4 @@ public class ToggleKeepScreenOnService extends TileService {
         return false;
     }
 
-    private void updateTile() {
-        final Tile tile = getQsTile();
-
-        if (isKeepScreenOnEnabled()) {
-            tile.setState(Tile.STATE_ACTIVE);
-        } else {
-            tile.setState(Tile.STATE_INACTIVE);
-        }
-
-        tile.updateTile();
-
-    }
 }
