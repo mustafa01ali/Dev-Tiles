1b91bc7b65dc135738722d564f43a568ca1be921
Mir Mustafa Ali <mustafa01ali@gmail.com>
2017-01-13 16:25:22 +0530

    Initial implementation of the scale dialog

--- app/src/main/java/xyz/mustafaali/devqstiles/service/ToggleAnimatorDurationService.java
+++ app/src/main/java/xyz/mustafaali/devqstiles/service/ToggleAnimatorDurationService.java
@@ -16,22 +16,31 @@
 
 package xyz.mustafaali.devqstiles.service;
 
+import android.content.DialogInterface;
 import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.view.ContextThemeWrapper;
 
+import xyz.mustafaali.devqstiles.R;
 import xyz.mustafaali.devqstiles.util.AnimatorDurationScaler;
 
 import static xyz.mustafaali.devqstiles.util.AnimatorDurationScaler.getAnimatorScale;
 import static xyz.mustafaali.devqstiles.util.AnimatorDurationScaler.getIcon;
 
 /**
- * A {@link TileService quick settings tile} for scaling animation durations. Toggles between 1x and
- * 5x animator duration scales.
+ * A {@link TileService quick settings tile} for scaling animation durations. Toggles between 0.5x and
+ * 10x animator duration scales.
  */
 public class ToggleAnimatorDurationService extends TileService {
 
-    public ToggleAnimatorDurationService() { }
+    private final CharSequence[] choices = {"Animation off", "Animation scale .5x", "Animation scale 1x",
+            "Animation scale 1.5x", "Animation scale 2x", "Animation scale 5x", "Animation scale 10x"};
+    private final float[] scales = {0f, 0.5f, 1f, 1.5f, 2f, 5f, 10f};
+
+    public ToggleAnimatorDurationService() {
+    }
 
     @Override
     public void onStartListening() {
@@ -42,9 +51,10 @@ public class ToggleAnimatorDurationService extends TileService {
     @Override
     public void onClick() {
         final float current = getAnimatorScale(getContentResolver());
-        final float target = current == 1f ? 5f : 1f;
-        AnimatorDurationScaler.setAnimatorScale(this, target);
-        updateTile();
+        showDialog(getDialog(1));
+//        final float target = current == 1f ? 5f : 1f;
+//        AnimatorDurationScaler.setAnimatorScale(this, target);
+//        updateTile();
     }
 
     private void updateTile() {
@@ -54,6 +64,20 @@ public class ToggleAnimatorDurationService extends TileService {
         tile.updateTile();
     }
 
+    private AlertDialog getDialog(int selectedIndex) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AppTheme_Dialog));
+        builder.setTitle(R.string.dialog_animator_duration_title)
+                .setSingleChoiceItems(choices, selectedIndex, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AnimatorDurationScaler.setAnimatorScale(ToggleAnimatorDurationService.this, scales[which]);
+                        updateTile();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null);
+        return builder.create();
+    }
 }
 
 
