f607b7a8105df2087863709e5766ffc46599a863
Mustafa Ali <mustafa.ali@shopify.com>
2017-09-15 10:43:51 -0400

    Updated Gradle and Kotlin deps, minor cleanup, made the commands text selectable to allow copy/paste

--- app/src/main/kotlin/xyz/mustafaali/devqstiles/service/ToggleShowTapsService.kt
+++ app/src/main/kotlin/xyz/mustafaali/devqstiles/service/ToggleShowTapsService.kt
@@ -2,33 +2,30 @@ package xyz.mustafaali.devqstiles.service
 
 import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
-
-import xyz.mustafaali.devqstiles.R
 
 class ToggleShowTapsService : BaseTileService() {
-    val TAG = this.javaClass.simpleName
-    val SHOW_TOUCHES = "show_touches"
+    private val TAG = this.javaClass.simpleName
+    private val SHOW_TOUCHES = "show_touches"
 
     override fun onClick() {
         val newValue = if (isFeatureEnabled()) 0 else 1
 
         try {
             Settings.System.putInt(contentResolver, SHOW_TOUCHES, newValue)
-        } catch (se: SecurityException) {
+        } catch (e: Exception) {
             showPermissionError()
-            Log.e(TAG, se.message)
+            Log.e(TAG, e.message)
         }
 
         updateTile()
     }
 
     override fun isFeatureEnabled(): Boolean {
-        try {
-            return Settings.System.getInt(contentResolver, SHOW_TOUCHES) == 1
+        return try {
+            Settings.System.getInt(contentResolver, SHOW_TOUCHES) == 1
         } catch (e: Settings.SettingNotFoundException) {
             e.printStackTrace()
-            return false
+            false
         }
     }
 }
